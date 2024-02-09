9eb7a3aa8d9cf697f5a8158e58395c2c70ab8e2f
Ewan Jee <830jiforcareer@gmail.com>
2024-02-09 13:29:32 -0500

    feat 스트림 메소드1

--- LearningLambda/src/programming/FP02Structured.java
+++ LearningLambda/src/programming/FP02Structured.java
@@ -5,8 +5,13 @@ import java.util.List;
 public class FP02Structured {
     public static void main(String[] args){
         List<Integer> numbers = List.of(12,9,13,4,6,2,4,12,15);
-        int sum = addListStructured(numbers);
-        System.out.println(sum);
+        //int sum = addListStructured(numbers);
+        //System.out.println(sum);
+        numbers
+                .stream()
+                .distinct()
+                .sorted()
+                .forEach(x -> System.out.println(x));
     }
 
     private static int addListStructured(List<Integer> numbers) {
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# Java
-자바를 배우는 저장소입니다
+# Java 
+
+## LearningLambda
+람다와 스트림 학습
+### method reference
+stream()
+        .map(x -> x + x) : 데이터 변경, x를 받아서 x+x로 변경
+        .filter(x -> x%2==0) : 데이터 필터링, x를 받아서 x%2==0인 것만 필터링 나머지는 없어짐
+        .reduce(0, (x, y) -> x + y) : 데이터 집계, x와 y를 받아서 x+y로 집계 x는 초기값 0
+        .distinct() : 중복 제거
+        .sorted() : 정렬
+        .forEach(x -> System.out.println(x)) : 데이터 출력, x를 받아서 출력
